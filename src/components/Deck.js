177043b4e571f9ed269c4d6a0f52c170bc09e4af
Dan Rosen <drosen@precipart.com>
2020-05-31 09:59:16 -0400

    fetching new cards

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -1,22 +1,49 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 
-const API_URL = 'https://deckofcardsapi.com/api/deck/new/shuffle'
+const API_BASE_URL = 'https://deckofcardsapi.com/api/deck'
 
 class Deck extends Component {
     constructor(props) {
         super(props)
 
         this.state = {
-            deck: null
+            deck: null,
+            drawn: []
         }
+
+        this.getCard = this.getCard.bind(this)
     }
 
     async componentDidMount() {
-        let deck = await axios.get(API_URL)
+        let deck = await axios.get(`${API_BASE_URL}/new/shuffle/`)
         this.setState({
             deck: deck.data
         })
+    }
+
+    async getCard() {
+        let id = this.state.deck.deck_id
+        try {
+            let cardUrl = `${API_BASE_URL}/${id}/draw/`
+            let cardRes = await axios.get(cardUrl)
+            if (!cardRes.data.success) {
+                throw new Error('No cards remaining!')
+            }
+            let card = cardRes.data.cards[0]
+            this.setState((st) => ({
+                drawn: [
+                    ...st.drawn,
+                    {
+                        id: card.code,
+                        image: card.image,
+                        name: `${card.value} of ${card.suit}`
+                    }
+                ]
+            }))
+        } catch (error) {
+            alert(error)
+        }
     }
 
     render() {
@@ -24,6 +51,7 @@ class Deck extends Component {
             <div>
                 <div>
                     <h1>Deck of Cards</h1>
+                    <button onClick={this.getCard}>Deal a Card!</button>
                 </div>
             </div>
         )
